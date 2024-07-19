492d02ab671cb1aed1f12d860cbb2c45b2877fa3
David Colarusso <colarusso@users.noreply.github.com>
2024-07-19 10:53:21 -0600

    improvements to tts, including variable reading speeds

--- js/tts.js
+++ js/tts.js
@@ -1,21 +1,24 @@
+const wordsPerMinute = localStorage.getItem('wpm') || 200;
+
 var voices = window.speechSynthesis.getVoices();
 
 var speaking = 0;
 
-var sayit = function(sentences, i) {
+var sayit = function(text) {
     var msg = new SpeechSynthesisUtterance();
 
     msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
-    msg.rate = 1; //localStorage.getItem('wpm')/175; //$('#rate').val(); // 0.1 to 10
+    msg.rate = localStorage.getItem('wpm')/175; //$('#rate').val(); // 0.1 to 10
     msg.pitch = 1; //0 to 2
     //msg.lang = 'en-US' //$('#lang').val();
+    msg.text = text;
+
     msg.onstart = function(event) {
         speaking = 1;
     };
     msg.onend = function(event) {
         console.log('Finished in ' + event.elapsedTime + ' seconds.');
-        // Check if there are more speeches queued, if not set speaking to 0
         if (speechSynthesis.speaking === false) {
             speaking = 0;
         }
@@ -34,20 +37,24 @@ var sayit = function(sentences, i) {
 }
 
 var talk = function(text) {
-    sentences = text.match(/[^\.!\?]+[\.!\?]+/g);
+    var sentences = text.match(/[^\.!\?]+[\.!\?]+/g);
 
     speechSynthesis.cancel(); // if it errors, this clears out the error.
 
-    for (var i = 0; i < sentences.length; i++) {
-        var toSay = sayit(sentences, i);
-        toSay.text = sentences[i];
+    if (sentences.length > 0) {
+        var toSay = sayit(sentences[0]);
+        speechSynthesis.speak(toSay);
+    }
+
+    for (var i = 1; i < sentences.length; i++) {
+        var toSay = sayit(sentences[i]);
         speechSynthesis.speak(toSay);
     }
 }
 
 var stop_talk = function() {
-    speechSynthesis.cancel();
     speaking = 0;
+    speechSynthesis.cancel();
 }
 
 var audio_play = function(text) {
